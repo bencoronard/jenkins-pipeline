c54f577afa92d5eaaaffe1bec60b290f4510cea8
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-09-30 15:45:22 +0700

    Init docker utility

---
+++ vars/docker.groovy
@@ -0,0 +1,39 @@
+def buildImage(Map args = [:]) {
+  withCredentials([
+    string(credentialsId: args.registry, variable: 'HOST')
+  ]) {
+    docker.withRegistry(constructRegistryUrl("${HOST}"), args.credentialsId) {
+      return docker.build(
+        "${HOST}/${args.name}:${args.tag}",
+        "--build-arg -f ${args.dockerfile} ENV=${args.environment}"
+      )
+    }
+  }
+}
+
+def pushToRegistry(Map args = [:]) {
+  withCredentials([
+    string(credentialsId: args.registry, variable: 'HOST')
+  ]) {
+    docker.withRegistry(constructRegistryUrl("${HOST}"), args.credentialsId) {
+      args.image.push()
+    }
+  }
+}
+
+def execInsideDocker(Map args = [:], Closure closure) {
+  withCredentials([
+    string(credentialsId: args.registry, variable: 'HOST')
+  ]) {
+    def fullImageName = "${HOST}/${args.image}"
+    docker.withRegistry(constructRegistryUrl("${HOST}"), args.credentialsId) {
+      docker.image(fullImageName).inside {
+        closure.call()
+      }
+    }
+  }
+}
+
+def constructRegistryUrl(String host) {
+  return "https://${host}"
+}
