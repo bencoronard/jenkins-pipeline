f0bc0e709fa55cf63d93a2ca4ff2b13bf66f3894
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-10-15 14:24:09 +0700

    Add addTagToExistingImage in harborUtil

--- vars/harborUtil.groovy
+++ vars/harborUtil.groovy
@@ -13,3 +13,29 @@ def checkImageExistence(Map args = [:]) {
     )
   }
 }
+
+def addTagToExistingImage(Map args = [:]) {
+  withCredentials([
+    string(credentialsId: args.registry, variable: 'HOST')
+  ]) {
+    def imageEncoded = URLEncoder.encode(args.image, 'UTF-8')
+    def tagEncoded = URLEncoder.encode(args.tag, 'UTF-8')
+    def newTagEncoded = URLEncoder.encode(args.newTag, 'UTF-8')
+    def response = httpRequest(
+      httpMode: 'GET',
+      url: "https://${HOST}/v2/${imageEncoded}/manifests/${tagEncoded}",
+      authentication: args.credentialsId,
+      customHeaders: [[name: 'Accept', value: 'application/vnd.docker.distribution.manifest.v2+json']],
+      ignoreSslErrors: true
+    )
+    httpRequest(
+      httpMode: 'PUT',
+      url: "https://${HOST}/v2/${imageEncoded}/manifests/${newTagEncoded}",
+      authentication: args.credentialsId,
+      customHeaders: [[name: 'Content-Type', value: 'application/vnd.docker.distribution.manifest.v2+json']],
+      requestBody: response.content,
+      ignoreSslErrors: true,
+      responseHandle: 'NONE'
+    )
+  }
+}
